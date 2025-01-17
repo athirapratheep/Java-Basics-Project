743607015695e1435585856fc3091571223cbf2f
AT <athirapratheep2002@gmail.com>
2025-01-17 21:15:37 +0530

    Create ArrayStatisticsVisualizer.java

---
+++ ArrayStatisticsVisualizer.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ArrayStatisticsVisualizer {
+    Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("-Array Statistics Visualizer-");
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(" Menu:");
+
+    }
+}   
