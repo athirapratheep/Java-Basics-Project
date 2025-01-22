eb6d47bbdbcf7e49f1fef306c8a119f232f4da40
AT <athirapratheep2002@gmail.com>
2025-01-22 19:18:36 +0530

    Update ArrayStatisticsVisualizer.java

--- ArrayStatisticsVisualizer.java
+++ ArrayStatisticsVisualizer.java
@@ -1,7 +1,12 @@
 import java.util.*;
+import javax.swing.*;
 public class ArrayStatisticsVisualizer {
     Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
+        JFrame f=new JFrame();
+        f.setSize(400,300);
+        f.setLayout(null);
+        
         Scanner sc=new Scanner(System.in);
         System.out.println("-Array Statistics Visualizer-");
         System.out.println("Enter the size of the array:");
