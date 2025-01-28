1454143027902de64c35df856204e2c65a3ec3ce
AT <athirapratheep2002@gmail.com>
2025-01-28 22:55:10 +0530

    Create StringPatternSearch.java

---
+++ StringPatternSearch/StringPatternSearch.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+public class StringPatternSearch {
+    public static void main(String[] args)
+    {
+        JFrame f=new JFrame("String Pattern Searcher");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+        f.setSize(500,400);
+        f.setLayout(new BorderLayout());
+
+        JLabel stringLabel = new JL
+    }
+}
